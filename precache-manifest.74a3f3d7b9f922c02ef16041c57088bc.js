34131a946898cabcd2d263be53211ead7c5b6e64
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-03 22:13:26 +0100

    Updates

---
+++ precache-manifest.74a3f3d7b9f922c02ef16041c57088bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e5500bc5d335b00583545b4ed83411",
+    "url": "/login004/index.html"
+  },
+  {
+    "revision": "d2adb376eca0cb0afbb6",
+    "url": "/login004/static/css/main.b8c71231.chunk.css"
+  },
+  {
+    "revision": "15e88ad842ebe3b2f0d0",
+    "url": "/login004/static/js/2.5a40f7b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login004/static/js/2.5a40f7b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2adb376eca0cb0afbb6",
+    "url": "/login004/static/js/main.36f57d91.chunk.js"
+  },
+  {
+    "revision": "55a80a3ceceacaeaeea8",
+    "url": "/login004/static/js/runtime-main.f779b495.js"
+  },
+  {
+    "revision": "9ef697cfc0544f81ba1b09d808163c01",
+    "url": "/login004/static/media/Wave.9ef697cf.png"
+  },
+  {
+    "revision": "5911f7233c89e52f6fd9f87c0ea68556",
+    "url": "/login004/static/media/undraw_authentication_fsn5.5911f723.svg"
+  },
+  {
+    "revision": "597d52770164c5086c5d791e8ee09b56",
+    "url": "/login004/static/media/undraw_fingerprint_swrc.597d5277.svg"
+  },
+  {
+    "revision": "d7b83c266483fc2d21116becc4a66172",
+    "url": "/login004/static/media/undraw_personal_data_29co.d7b83c26.svg"
+  }
+]);
